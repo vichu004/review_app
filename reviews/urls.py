edbef208994cb37e91dda1d9c582d54da147635a
vichu004 <vishnusakthi004@gmail.com>
2025-05-21 10:19:36 +0530

    made changes in the urls.py file to make the link redirections

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,3 +1,6 @@
-# from django.urls import path
-
-# patterns=[]
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.review,name="REVIEW"),
+    path('thankyou',views.thankyou,name="THANK_YOU")
+]
